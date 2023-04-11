f87dcb1e9a8c1e5cdf6161d7aae9d52326ab5b6e
DhavalMalsattar <122300767+DhavalMalsattar@users.noreply.github.com>
2023-04-11 23:06:54 +0530

    added LLinC

---
+++ LLinC.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+struct LL{
+    struct node *head;
+    head=(struct node*) malloc(sizeof(struct node));
+    head->next=0;
+    
+};
+void addlast(struct LL list,int data){
+        struct node *n1;
+        n1=(struct node*) malloc(sizeof(struct node));
+        n1->data=data;
+        n1->next=0;
+        struct node* temp=list.head;
+        if(list.head->next==0){
+            list.head=n1;
+            return;
+        }
+        while(temp->next!=0){
+            temp=temp->next;
+        }
+        temp->next=n1;
+    }
+
+    void print(struct LL list){
+        struct node*ptr=list.head;
+        while(ptr!=NULL){
+            printf("%d ",(*ptr).data);
+            ptr=ptr->next;
+        }    
+    }
+
+int main(){
+    
+    struct LL list;
+    //printf("%d",list.head->next);
+    addlast(list,2);
+    addlast(list,4);
+    addlast(list,6);
+    addlast(list,7);
+    addlast(list,9);
+    
+
+    print(list);
+
+    return 0;
+}
